ca67501089f631102fcb651fa3f33e774022ce74
James Wenzel <wenzel.james.r@gmail.com>
2022-03-17 11:16:46 -0700

    use safetransferlib

--- src/util/Withdrawable.sol
+++ src/util/Withdrawable.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 import {Ownable} from "oz/access/Ownable.sol";
-import {IERC20} from "oz/interfaces/IERC20.sol";
 import {IWithdrawable} from "./IWithdrawable.sol";
+import {SafeTransferLib} from "sm/utils/SafeTransferLib.sol";
+import {ERC20} from "sm/tokens/ERC20.sol";
 
 ///@notice Ownable helper contract to withdraw ether or tokens from the contract address balance
 contract Withdrawable is IWithdrawable, Ownable {
@@ -13,13 +14,14 @@ contract Withdrawable is IWithdrawable, Ownable {
     ///@notice Withdraw Ether from contract address. OnlyOwner.
     function withdraw() external override onlyOwner {
         uint256 balance = address(this).balance;
-        payable(msg.sender).transfer(balance);
+        SafeTransferLib.safeTransferETH(msg.sender, balance);
     }
 
     ///@notice Withdraw tokens from contract address. OnlyOwner.
     ///@param _token ERC20 smart contract address
     function withdrawToken(address _token) external override onlyOwner {
-        uint256 balance = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(msg.sender, balance);
+        ERC20 token = ERC20(_token);
+        uint256 balance = ERC20(_token).balanceOf(address(this));
+        SafeTransferLib.safeTransfer(token, msg.sender, balance);
     }
 }
